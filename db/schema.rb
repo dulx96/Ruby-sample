0e5e0d83b51cdb1a4a653236bffc7c50b2733889
thm1707 <tranhoangminh1707@gmail.com>
2017-11-09 21:05:30 +0700

    Advance Login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171011153003) do
+ActiveRecord::Schema.define(version: 20171109085534) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20171011153003) do
     t.string "password_digest"
     t.integer "gender"
     t.date "birth"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
